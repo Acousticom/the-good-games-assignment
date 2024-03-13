02307f29f120ee8c17a60484bf37eac22b1b93fd
Acousticom <omkarsrinivasa.2000@gmail.com>
2024-03-13 21:47:44 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import ProductsCard from "./components/ProductsCard";
 import ShimmerCard from "./components/ShimmerCard";
 import Navbar from "./components/Navbar";
 function App() {
-  const {loading,filterProductsBySearch } = useData();
+  const {loading,filterProductsBySearch,inputSearch } = useData();
   const filteredProducts=filterProductsBySearch()
   return (
     <div className="">
         <Navbar />
       <div className="flex flex-wrap justify-center gap-4 mt-24">
         {loading && [...Array(8)].map((arr) => <ShimmerCard />)}
-        {filteredProducts?.length>0?filteredProducts?.map((data) => (
+        {filteredProducts?.map((data) => (
           <ProductsCard data={data} key={data.id} />
-        )):<h1 className="text-2xl font-bold">Not found</h1>}
+        ))}
+        {(filteredProducts?.length===0&&inputSearch?.length>0)?(<h1 className="text-2xl font-bold">Not found</h1>):null}
+
       </div>
     </div>
   );
--- src/components/ProductsCard.jsx
+++ src/components/ProductsCard.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 const ProductsCard = ({data}) => {
   const {name,image_url,description,ingredients}=data
-  console.log(ingredients)
   return (
     <div className=' border-2 border-gray-300 w-[18rem] flex flex-col items-center py-6 px-3 rounded-lg shadow-xl'>
       <img src={image_url} alt="" className='h-[10rem] mb-3'/>
